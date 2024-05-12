1b8f5881f89a1a0304ca863cba0c7ac587f59cd5
Чивапчичи <125189325+Peresmeshnik01@users.noreply.github.com>
2024-05-12 10:12:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ import sys
 import os
 import rez1
 from quiz1 import questions, guesses, options, answers, images, font, display_question
-
+from menu import menu_anim
+import time
 pygame.init()
 
-# Инициализация окна
-screen = pygame.display.set_mode((1000, 522))
-pygame.display.set_caption("Quiz Game")
-
-# Цвета
 WHITE = (255, 255, 255)
 BLACK = (255, 255, 0)
 GRAY = (150, 150, 150)
 
 running = True
-image_positions = [(70, 50), (520, 50)]  # Пример координат для двух изображений
+image_positions = [(70, 50), (520, 50)]  
 questions_num = 0
 
+menu_anim()
+
+time.sleep(1)
+screen = pygame.display.set_mode((1000, 522))
+pygame.display.set_caption("Quiz Game")
+pygame.mixer.music.load('Quiz1.mp3')
+pygame.mixer.music.play(-1)
 while running and questions_num < len(questions):
     current_question = questions[questions_num]
     current_options = options[questions_num]
     current_images = images[questions_num]
-    display_question(screen, current_question, current_options, current_images, image_positions)
 
+    display_question(screen, current_question, current_options, current_images, image_positions)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
