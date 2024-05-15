d667ae3a7f74db92c75e13c1bb4f68da55a84653
Чивапчичи <125189325+Peresmeshnik01@users.noreply.github.com>
2024-05-15 22:57:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import pygame
 import sys
 import os
-import rez1
-from quiz1 import questions, guesses, options, answers, images, font, display_question
 from menu import menu_anim
 import time
+from TestQuiz import run_game
 pygame.init()
 
 WHITE = (255, 255, 255)
 BLACK = (255, 255, 0)
 GRAY = (150, 150, 150)
 
-running = True
-image_positions = [(70, 50), (520, 50)]  
-questions_num = 0
 
 menu_anim()
 
@@ -22,25 +18,14 @@ screen = pygame.display.set_mode((1000, 522))
 pygame.display.set_caption("Quiz Game")
 pygame.mixer.music.load(os.path.join("sounds", 'Quiz1.mp3'))
 pygame.mixer.music.play(-1)
-while running and questions_num < len(questions):
-    current_question = questions[questions_num]
-    current_options = options[questions_num]
-    current_images = images[questions_num]
 
-    display_question(screen, current_question, current_options, current_images, image_positions)
-    
+running = True
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            for i, option in enumerate(current_options):
-                text_rect = font.render(option, True, BLACK).get_rect(topleft=(50 if i == 0 else 500, 150 + i))
-                if text_rect.collidepoint(pygame.mouse.get_pos()):
-                    choice = option
+        run_game()  
 
-                    questions_num += 1
-            file_path = 'quiz1_result.csv'
-            rez1.save_results_to_csv(guesses, answers, file_path)
 pygame.quit()
 sys.exit()
-
