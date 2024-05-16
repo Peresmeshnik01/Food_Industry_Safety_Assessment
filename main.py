5e4f315aff771c00df51f8210dcf723089fb6140
Чивапчичи <125189325+Peresmeshnik01@users.noreply.github.com>
2024-05-16 22:51:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import os
 from menu import menu_anim
 import time
-from TestQuiz import run_game
+from TestQuiz import display_question, screen, questions, questions_num, options, images, buttons, save_image
 pygame.init()
 
 WHITE = (255, 255, 255)
@@ -19,13 +19,28 @@ pygame.display.set_caption("Quiz Game")
 pygame.mixer.music.load(os.path.join("sounds", 'Quiz1.mp3'))
 pygame.mixer.music.play(-1)
 
+
 running = True
 
 while running:
+    global questions_num
+    if questions_num < len(questions):
+        display_question(screen, questions[questions_num], options, images, questions_num)
+    
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        run_game()  
-
-pygame.quit()
-sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            for i, button in enumerate(buttons[questions_num]):
+                if button.rect.collidepoint(event.pos):
+                    user_answer = options[questions_num][i]
+                    save_image(user_answer, questions_num)
+                    questions_num += 1
+                     
+    if questions_num >= len(questions):
+        print("Вопросы закончились")
+            
+        running = False
+        pygame.quit()
+            #pygame.display.flip()
