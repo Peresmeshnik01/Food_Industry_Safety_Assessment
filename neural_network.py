ffec59c77c067c5fcd26af1aea42e6f60141bc87
Чивапчичи <125189325+Peresmeshnik01@users.noreply.github.com>
2024-05-19 23:21:11 +0700

    Add files via upload

---
+++ neural_network.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def create_cnn_model():
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        MaxPooling2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D(2, 2),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dense(2, activation='softmax')
+    ])
+    model.compile(optimizer=Adam(), loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
+    return model
+
+def train_model(train_dir, val_dir):
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=32, class_mode='binary')
+
+    val_datagen = ImageDataGenerator(rescale=1./255)
+    val_generator = val_datagen.flow_from_directory(val_dir, target_size=(150, 150), batch_size=32, class_mode='binary')
+
+    model = create_cnn_model()
+    model.fit(train_generator, steps_per_epoch=len(train_generator), epochs=20, validation_data=val_generator, validation_steps=len(val_generator))
+    return model
+
+def test_model(model, test_dir):
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(test_dir, target_size=(150, 150), batch_size=32, class_mode='binary', shuffle=False)
+    
+    loss, acc = model.evaluate(test_generator)
+    print(f"Test accuracy: {acc}")
+
+    predictions = model.predict(test_generator)
+    threshold = 0.5
+
+    filenames = test_generator.filenames  # Список имен файлов в генераторе
+    return predictions, filenames
+    #for i, prediction in enumerate(predictions):
+    #    if prediction[0] <= threshold:  # Предполагаемый класс - 0
+    #        print(f"Image {filenames[i]} classified as spolied")
+    #    else:
+    #        print(f"Image {filenames[i]} classified as fresh")
