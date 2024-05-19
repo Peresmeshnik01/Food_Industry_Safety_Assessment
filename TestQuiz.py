d4f2c3606ab744f5aabc6997399d22178bb0c73e
Чивапчичи <125189325+Peresmeshnik01@users.noreply.github.com>
2024-05-19 23:20:40 +0700

    Update TestQuiz.py

--- TestQuiz.py
+++ TestQuiz.py
@@ -2,7 +2,8 @@ import pygame
 import os
 import shutil
 import zipfile
-
+import neural_network
+import time
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 font = pygame.font.SysFont(None, 35)
@@ -10,7 +11,7 @@ font = pygame.font.SysFont(None, 35)
 questions = [
     "На каком из этих изображений присутствует свежий продукт питания?",
     "На каком из этих изображений присутствует свежий продукт питания?",
-    "Что из этого не является тортом?"
+    "На каком из этих изображений присутствует свежий продукт питания?"
 ]
 
 options = [
@@ -54,23 +55,19 @@ def display_question(screen, question, options, images, questions_num):
     screen.blit(background_static, (0, 0))
     screen.blit(bob, (0, 0))
     screen.blit(pat, (0, 0))
-    text_question = font.render(question, True, (255, 255, 0))
+    text_question = font.render(question, True, (236, 236, 0))
     screen.blit(text_question, (50, 50))
     if questions_num < len(questions):
         for i, option in enumerate(options[questions_num]):
             buttons[questions_num][i].draw(screen)
-
     pygame.display.flip()
 def save_image(user_answer, question_num):
     image_path = f"image{question_num+1}_{user_answer}.png"
-    #print(user_answer)
     if user_answer == '1':
         save_folder = inner_folder
         new_image_name = f"image{question_num+1}_{save_folder}.png"
         destination_path = os.path.join('fresh-food-vs-spoiled-food-classification', 'train', save_folder, new_image_name)
         shutil.copy(os.path.join('images', 'quiz1_mc', image_path), destination_path)
-        
-        print(f"Спанч Боб: {user_answer} относится к свежему продукту {save_folder}: {image_path}")
         
         save_folder = "spolied"
         user_answer = '2'
@@ -79,16 +76,12 @@ def save_image(user_answer, question_num):
         destination_path = os.path.join('fresh-food-vs-spoiled-food-classification', 'train', save_folder, new_image_name)
         shutil.copy(os.path.join('images', 'quiz1_mc', image_path), destination_path)
         
-        
-        print(f"Спанч Боб: {user_answer} относится к испорченному продукту {save_folder}: {image_path}")
     elif user_answer == '2':
         save_folder = inner_folder
         new_image_name = f"image{question_num+1}_{save_folder}.png"
         destination_path = os.path.join('fresh-food-vs-spoiled-food-classification', 'train', save_folder, new_image_name)
         shutil.copy(os.path.join('images', 'quiz1_mc', image_path), destination_path)
-       
-        print(f"Спанч Боб: {user_answer} относится к свежему продукту {save_folder}: {image_path}")
-        
+    
         save_folder = "spolied"
         user_answer = '1'
         image_path = f"image{question_num+1}_{user_answer}.png"
@@ -96,5 +89,114 @@ def save_image(user_answer, question_num):
         destination_path = os.path.join('fresh-food-vs-spoiled-food-classification', 'train', save_folder, new_image_name)
         shutil.copy(os.path.join('images', 'quiz1_mc', image_path), destination_path)
         
-        print(f"Спанч Боб: {user_answer} относится к испорченному продукту {save_folder}: {image_path}")
+def display_text_pat(screen, text):
+    background_static = pygame.image.load(os.path.join('images', 'background.jpg'))
+    bob = pygame.image.load(os.path.join('images', 'bob.png'))
+    pat = pygame.image.load(os.path.join('images', 'pat.png'))
+    screen.blit(background_static, (0, 0))
+    screen.blit(bob, (0, 0))
+    screen.blit(pat, (0, 0))
+    #screen.fill((255, 255, 255))
+    text_surface = font.render(text, True, (255,156,165))
+    text_rect = text_surface.get_rect(center=(700, 400))
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()
 
+def display_1Q(screen, text):
+    background_static = pygame.image.load(os.path.join('images', 'background.jpg'))
+    bob = pygame.image.load(os.path.join('images', 'bob.png'))
+    pat = pygame.image.load(os.path.join('images', 'pat.png'))
+    screen.blit(background_static, (0, 0))
+    screen.blit(bob, (0, 0))
+    screen.blit(pat, (0, 0))
+    image1_fresh = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test1/1Q', 'image1_fresh.png'))
+    image1_spoiled = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test1/1Q', 'image1_spolied.png'))
+
+    # Отображение изображения image1_fresh в координатах (50, 150)
+    screen.blit(image1_fresh, (150, 50))
+    # Отображение изображения image1_spoiled в координатах (500, 150)
+    screen.blit(image1_spoiled, (500, 50))
+    text_surface = font.render(text, True, (255,156,165))
+    text_rect = text_surface.get_rect(center=(700, 400))
+    screen.blit(text_surface, text_rect)
+
+    text_surface = font.render("Текст", True, (236, 236, 0))
+    text_rect = text_surface.get_rect(topleft=(50, 50))
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()
+def display_2Q(screen, text):
+    background_static = pygame.image.load(os.path.join('images', 'background.jpg'))
+    bob = pygame.image.load(os.path.join('images', 'bob.png'))
+    pat = pygame.image.load(os.path.join('images', 'pat.png'))
+    screen.blit(background_static, (0, 0))
+    screen.blit(bob, (0, 0))
+    screen.blit(pat, (0, 0))
+    image1_fresh = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test2/1Q', 'image2_fresh.png'))
+    image1_spoiled = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test2/1Q', 'image2_spolied.png'))
+
+    # Отображение изображения image1_fresh в координатах (50, 150)
+    screen.blit(image1_fresh, (150, 50))
+    # Отображение изображения image1_spoiled в координатах (500, 150)
+    screen.blit(image1_spoiled, (500, 50))
+    text_surface = font.render(text, True, (255,156,165))
+    text_rect = text_surface.get_rect(center=(700, 400))
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()
+def display_3Q(screen, text):
+    background_static = pygame.image.load(os.path.join('images', 'background.jpg'))
+    bob = pygame.image.load(os.path.join('images', 'bob.png'))
+    pat = pygame.image.load(os.path.join('images', 'pat.png'))
+    screen.blit(background_static, (0, 0))
+    screen.blit(bob, (0, 0))
+    screen.blit(pat, (0, 0))
+    image1_fresh = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test3/1Q', 'image3_fresh.png'))
+    image1_spoiled = pygame.image.load(os.path.join('fresh-food-vs-spoiled-food-classification/test3/1Q', 'image3_spolied.png'))
+
+    # Отображение изображения image1_fresh в координатах (50, 150)
+    screen.blit(image1_fresh, (150, 50))
+    # Отображение изображения image1_spoiled в координатах (500, 150)
+    screen.blit(image1_spoiled, (500, 50))
+    text_surface = font.render(text, True, (255,156,165))
+    text_rect = text_surface.get_rect(center=(700, 400))
+    screen.blit(text_surface, text_rect)
+    pygame.display.flip()
+def neural_work():
+    display_1Q(screen, "думаю...")
+    pygame.display.flip()
+    train_dir = 'fresh-food-vs-spoiled-food-classification/train'
+    val_dir = 'fresh-food-vs-spoiled-food-classification/val'
+    test_dir = 'fresh-food-vs-spoiled-food-classification/test1'
+    threshold = 0.5
+
+    model = neural_network.train_model(train_dir, val_dir)
+    predictions, filenames = neural_network.test_model(model, test_dir)  # Присваиваем возвращаемые значения переменным predictions и filenames
+    for i, prediction in enumerate(predictions):
+        if prediction[0] <= threshold:  # Предполагаемый класс - 0
+            display_1Q(screen, "справа тухлый")
+        else:
+            display_1Q(screen, "слева свежий")
+    time.sleep(2)
+
+    display_2Q(screen, "думаю...")
+    model = neural_network.train_model(train_dir, val_dir)
+    test_dir = 'fresh-food-vs-spoiled-food-classification/test2'
+    predictions, filenames = neural_network.test_model(model, test_dir)
+    for i, prediction in enumerate(predictions):
+        if prediction[0] <= threshold:  # Предполагаемый класс - 0
+            display_2Q(screen, "справа тухлый")
+        else:
+            display_2Q(screen, "слева свежий")
+    time.sleep(2)
+
+    display_3Q(screen, "думаю...")
+    model = neural_network.train_model(train_dir, val_dir)
+    test_dir = 'fresh-food-vs-spoiled-food-classification/test3'
+    predictions, filenames = neural_network.test_model(model, test_dir)
+    for i, prediction in enumerate(predictions):
+        if prediction[0] <= threshold:  # Предполагаемый класс - 0
+            display_3Q(screen, "справа тухлый")
+        else:
+            display_3Q(screen, "слева свежий")
+    time.sleep(4)
+    
+    pygame.quit()
